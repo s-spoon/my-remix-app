42dcdb1551703a0097f5be479c107fbfc61852ed
s-spoon <pswchttopwdm@gmail.com>
2025-06-09 11:54:48 -0500

    Update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
+  console.error("❌ DATABASE_URL is not defined in environment variables!");
+  throw new Error("Missing DATABASE_URL");
+} else {
+  console.log("✅ DATABASE_URL detected:", process.env.DATABASE_URL);
 }
 const prisma = new PrismaClient();
 export default prisma;
