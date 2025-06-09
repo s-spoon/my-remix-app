5f46ec357379016c7bfe2916abfc99fd1fc4d41b
s-spoon <pswchttopwdm@gmail.com>
2025-06-09 11:51:22 -0500

    Update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
 const prisma = new PrismaClient();
 export default prisma;
